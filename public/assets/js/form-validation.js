2fa1ef0a255aaf542b7ea82ef206c54f3bcf215e
Dikshita <dikshitasingh826@gmail.com>
2025-06-24 23:15:56 +0530

    form-validation

---
+++ public/assets/js/form-validation.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const confirmPasswordInput = document.getElementById("confirm_password");
+
+
+    const email_inputBox = document.getElementById("email_inputBox");
+    const password_InputBox = document.getElementById("password_inputBox");
+    const confirm_password_InputBox = document.getElementById("confirm_password_inputBox");
+
+    let pswd;
+
+    emailInput.addEventListener("keyup", (e) => {
+        validateEmail(e.target.value);
+    });
+
+    passwordInput.addEventListener("keyup", (e) => {
+        validatePassword(e.target.value);
+        pswd = e.target.value
+    });
+
+    confirmPasswordInput.addEventListener("keyup", (e) => {
+        validateConfirmPassword(e.target.value);
+    });
+
+    const validateEmail = (value) => {
+        const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/; 
+        if(emailPattern.test(value)){
+            console.log("passed");
+            email_inputBox.style.border = "1px solid rgba(0,0,0,0.12)"
+        }else{
+            console.log("failed");
+            email_inputBox.style.border = "2px solid red";
+        }
+    };
+
+    const validatePassword = (value) => {
+        const passwordPattern = /^(?=,*\d)(?=.*[A-Z]).{8,}$/;
+        if(passwordPattern.test(value)){
+            console.log("passed");
+            password_InputBox.style.border = "1px solid rgba(0,0,0,0.12)"
+        }else{
+            console.log("failed");
+            password_InputBox.style.border = "2px solid red";
+        }
+    };
+
+    const validateConfirmPassword = (value) => {
+        const passwordPattern = /^(?=,*\d)(?=.*[A-Z]).{8,}$/;
+        if(passwordPattern.test(value) && value === pswd){
+            console.log("passed");
+            confirm_password_InputBox.style.border = "1px solid rgba(0,0,0,0.12)"
+        }else{
+            console.log("failed");
+            confirm_password_InputBox.style.border = "2px solid red";
+        }
+    }
+});
